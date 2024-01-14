c73af6c32b43d1c36214d252d4247324f405dac1
Rosa Richter <cosmic.lady.rosa@gmail.com>
2024-01-13 23:00:11 -0700

    Use clap so i can do different stuff with the binary

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,43 @@
-use std::env;
-
 use waifu_calendar::character::Character;
 
 use anyhow::Result;
+use clap::{Parser, Subcommand};
 use time::{Duration, OffsetDateTime};
 use std::error::Error;
 
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+  #[command(subcommand)]
+  command: Option<Commands>
+}
+
+#[derive(Subcommand)]
+enum Commands {
+  Get {
+    username: String,
+  }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-  let username = env::args().collect::<Vec<String>>()[1].to_string();
+  let cli = Cli::parse();
 
+  match &cli.command {
+    Some(Commands::Get { username }) => {
+      print_birthday_table(username.to_string()).await?;
+    }
+    &None => {}
+  }
+
+  Ok(())
+}
+
+async fn print_birthday_table(username: String) -> Result<()> {
   println!("Fetching favorite character birthdays for username {}", username);
 
-  let now = OffsetDateTime::now_utc();
   let characters = waifu_calendar::get_waifu_birthdays(username).await?;
+  let now = OffsetDateTime::now_utc();
 
   let (characters_bd_today, characters_bd_future): (Vec<Character>, Vec<Character>) = characters.into_iter().partition(|character| {
     character.birthday.is_occurring_on(&now.date())
@@ -34,17 +58,21 @@ async fn main() -> Result<(), Box<dyn Error>> {
     next <= in_thirty_days.date()
   });
 
-  println!("\nUpcoming birthdays (next 30 days):\n");
+  if !characters_bd_next_month.is_empty() {
+    println!("\nUpcoming birthdays (next 30 days):\n");
 
-  characters_bd_next_month.iter().for_each(|character| {
-      println!("{}", character_row(character, &now));
-  });
+    characters_bd_next_month.iter().for_each(|character| {
+        println!("{}", character_row(character, &now));
+    });
+  }
 
-  println!("\nFuture birthdays:\n");
+  if !characters_bd_future.is_empty() {
+    println!("\nFuture birthdays:\n");
 
-  characters_bd_future.iter().for_each(|character| {
-      println!("{}", character_row(character, &now));
-  });
+    characters_bd_future.iter().for_each(|character| {
+        println!("{}", character_row(character, &now));
+    });
+  }
 
   Ok(())
 }
